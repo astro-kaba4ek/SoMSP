a8a8287d8d4cf2898c5ff187d7d760d8acc0c12e
sergey <st079380@student.spbu.ru>
2023-03-02 19:04:11 +0300

    n>1, почти

--- src/scattering_calculation.f90
+++ src/scattering_calculation.f90
@@ -51,11 +51,14 @@ contains
         integer(MPI_ADDRESS_KIND) :: displs(6), displs1(4), displs2(3) ! можно ли просто integer сделать (?)
         type(Node) :: Node1
         logical, allocatable :: node_logic(:)
-        integer, allocatable :: node_int(:), node_prev(:), m_array(:), nodes_previous(:,:), ij(:,:), MCR_ind(:)
-        integer :: sum_size, s, c, sum_size2, c1
+        integer, allocatable :: node_int(:), node_prev(:), nodes_previous(:,:), ij(:,:), MCR_ind(:)
+        integer :: sum_size, s, c, sum_size2, c1, qlen_m(minm:maxm), m_array(maxm-minm+1), k
         type(ModeInfo) :: node_info
         real(knd), allocatable :: MCR_real(:)
         complex(knd), allocatable :: MCR_comp_mat(:), MCR_comp_arr(:)
+        type(Node), allocatable :: queue_m(:,:)
+        type(ModeCalculationResult), allocatable :: mode_res_m(:,:)
+
 
         call res%initialize()
 
@@ -106,7 +109,7 @@ contains
 
         accuracy = 0
 
-        allocate(m_array(maxm-minm+1))
+        ! allocate(m_array(maxm-minm+1))
         m_array = [(m, m=minm, maxm)]
         ! m начинается с 0 (0, 1, 2), потоков на 1 больше (4)
         ! do m = minm, maxm
@@ -119,7 +122,7 @@ contains
             call typed_model%build_mode_queue(m, lnum, spherical_lnum, queue)
             if (LOG_INFO) write(LOG_FD,*) 
             qlen = size(queue)
-            print*, "aa", qlen
+            print*, "aa", qlen, rank, m
             if (qlen == 0) then
                 ! cycle
             endif
@@ -128,7 +131,7 @@ contains
             ! accuracy = 0
             mode_res = calculate_m(scattering_context, queue, m, lnum, sph_lnum)
             
-            print*, "lol"
+            print*, "lol", rank, m
             ! print*, 1, mode_res(1)%factors%Qext
             ! print*, 1, mode_res(1)%factors%Qsca
             ! print*, 1, mode_res(1)%tmatrix(3,5)
@@ -187,9 +190,12 @@ contains
                 node_prev = [node_prev, size(queue(i)%previous), queue(i)%previous]
             end do
 
-            call MPI_Ssend(node_logic, 2*qlen, MPI_LOGICAL, 0, Rank, MPI_COMM_WORLD, Err)
-            call MPI_Ssend(node_int, (2+4)*qlen, MPI_INTEGER, 0, Rank, MPI_COMM_WORLD, Err)
-            call MPI_Ssend(node_prev, sum_size, MPI_INTEGER, 0, Rank, MPI_COMM_WORLD, Err)
+            call MPI_Ssend(qlen, 1, MPI_INTEGER, 0, m, MPI_COMM_WORLD, Err)
+            call MPI_Barrier(MPI_COMM_WORLD, Err)
+
+            call MPI_Ssend(node_logic, 2*qlen, MPI_LOGICAL, 0, m, MPI_COMM_WORLD, Err)
+            call MPI_Ssend(node_int, (2+4)*qlen, MPI_INTEGER, 0, m, MPI_COMM_WORLD, Err)
+            call MPI_Ssend(node_prev, sum_size, MPI_INTEGER, 0, m, MPI_COMM_WORLD, Err)
 
             deallocate(node_logic, node_int, node_prev)
 
@@ -223,40 +229,66 @@ contains
             end do
 
             if (knd == 16) then
-                call MPI_Ssend(MCR_real, 2*qlen, MPI_REAL16, 0, Rank, MPI_COMM_WORLD, Err)
-                call MPI_Ssend(MCR_comp_mat, s, MPI_COMPLEX16, 0, Rank, MPI_COMM_WORLD, Err)
-                call MPI_Ssend(MCR_comp_arr, sum_size2, MPI_COMPLEX16, 0, Rank, MPI_COMM_WORLD, Err)
+                call MPI_Ssend(MCR_real, 2*qlen, MPI_REAL16, 0, m, MPI_COMM_WORLD, Err)
+                call MPI_Ssend(MCR_comp_mat, s, MPI_COMPLEX16, 0, m, MPI_COMM_WORLD, Err)
+                call MPI_Ssend(MCR_comp_arr, sum_size2, MPI_COMPLEX16, 0, m, MPI_COMM_WORLD, Err)
             elseif (knd == 8) then
-                call MPI_Ssend(MCR_real, 2*qlen, MPI_REAL8, 0, Rank, MPI_COMM_WORLD, Err)
-                call MPI_Ssend(MCR_comp_mat, s, MPI_COMPLEX8, 0, Rank, MPI_COMM_WORLD, Err)
-                call MPI_Ssend(MCR_comp_arr, sum_size2, MPI_COMPLEX8, 0, Rank, MPI_COMM_WORLD, Err)
+                call MPI_Ssend(MCR_real, 2*qlen, MPI_REAL8, 0, m, MPI_COMM_WORLD, Err)
+                call MPI_Ssend(MCR_comp_mat, s, MPI_COMPLEX8, 0, m, MPI_COMM_WORLD, Err)
+                call MPI_Ssend(MCR_comp_arr, sum_size2, MPI_COMPLEX8, 0, m, MPI_COMM_WORLD, Err)
             end if
-            call MPI_Ssend(MCR_ind, 3*qlen, MPI_INTEGER, 0, Rank, MPI_COMM_WORLD, Err)
+            call MPI_Ssend(MCR_ind, 3*qlen, MPI_INTEGER, 0, m, MPI_COMM_WORLD, Err)
 
             deallocate(ij, MCR_real, MCR_comp_mat, MCR_comp_arr, MCR_ind)
 
      
+            ! печать промежуточных вычислений на экран
+            call typed_model%print_mode_row(m, mode_res)
+
+
             deallocate(queue, mode_res)
 
+
+
         else!if (Rank == 0) then
+            ! print*, "lollol", rank, m
 
+
+            ! собираем массив с информацией о длиннах массивов на каждой итерации цикла
+            do k = 1, size(m_array)
+
+                call MPI_Recv(qlen, 1, MPI_INTEGER, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err)
+
+                ! получаем индефикатор потока, то есть m
+                m = Status%MPI_TAG
+                qlen_m(m) = qlen
+            ! print*, "lollol",k, rank, m
+
+            end do
+            call MPI_Barrier(MPI_COMM_WORLD, Err)
+
+            ! print*, "kek", rank, m
+
+            allocate(mode_res_m(maxval(qlen_m),minm:maxm), queue_m(maxval(qlen_m),minm:maxm))
+
+            do k = 1, size(m_array)
             ! принимаем шифрованный queue
+            
             call MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
-            call MPI_Get_count(Status, MPI_LOGICAL, qlen, Err)
-
-            qlen = qlen / 2
+            m = Status%MPI_TAG
+            qlen = qlen_m(m)
 
             allocate(node_logic(2*qlen))
-            call MPI_Recv(node_logic, 2*qlen, MPI_LOGICAL, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
+            call MPI_Recv(node_logic, 2*qlen, MPI_LOGICAL, MPI_ANY_SOURCE, m, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
 
             allocate(node_int((2+4)*qlen))
-            call MPI_Recv(node_int, (2+4)*qlen, MPI_INTEGER, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
+            call MPI_Recv(node_int, (2+4)*qlen, MPI_INTEGER, MPI_ANY_SOURCE, m, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
 
-            call MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
+            call MPI_Probe(MPI_ANY_SOURCE, m, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
             call MPI_Get_count(Status, MPI_INTEGER, sum_size, Err)
 
             allocate(node_prev(sum_size))
-            call MPI_Recv(node_prev, sum_size, MPI_INTEGER, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
+            call MPI_Recv(node_prev, sum_size, MPI_INTEGER, MPI_ANY_SOURCE, m, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
 
           
         ! nodes_previous
@@ -309,42 +341,43 @@ contains
             
             deallocate(node_prev, node_logic, node_int)
 
+            queue_m(1:,m) = queue
 
 
             ! принимаем шифорванный mode_res
             if (knd == 16) then
                 allocate(MCR_real(2*qlen))
-                call MPI_Recv(MCR_real, 2*qlen, MPI_REAL16, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
+                call MPI_Recv(MCR_real, 2*qlen, MPI_REAL16, MPI_ANY_SOURCE, m, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
     
-                call MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
+                call MPI_Probe(MPI_ANY_SOURCE, m, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
                 call MPI_Get_count(Status, MPI_COMPLEX16, s, Err)
 
                 allocate(MCR_comp_mat(s))
-                call MPI_Recv(MCR_comp_mat, s, MPI_REAL16, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
+                call MPI_Recv(MCR_comp_mat, s, MPI_REAL16, MPI_ANY_SOURCE, m, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
     
-                call MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
+                call MPI_Probe(MPI_ANY_SOURCE, m, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
                 call MPI_Get_count(Status, MPI_COMPLEX16, sum_size2, Err)
 
                 allocate(MCR_comp_arr(sum_size2))
-                call MPI_Recv(MCR_comp_arr, sum_size2, MPI_REAL16, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
+                call MPI_Recv(MCR_comp_arr, sum_size2, MPI_REAL16, MPI_ANY_SOURCE, m, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
             elseif (knd == 8) then
                 allocate(MCR_real(2*qlen))
-                call MPI_Recv(MCR_real, 2*qlen, MPI_REAL8, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
+                call MPI_Recv(MCR_real, 2*qlen, MPI_REAL8, MPI_ANY_SOURCE, m, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
     
-                call MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
+                call MPI_Probe(MPI_ANY_SOURCE, m, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
                 call MPI_Get_count(Status, MPI_COMPLEX8, s, Err)
 
                 allocate(MCR_comp_mat(s))
-                call MPI_Recv(MCR_comp_mat, s, MPI_COMPLEX8, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
+                call MPI_Recv(MCR_comp_mat, s, MPI_COMPLEX8, MPI_ANY_SOURCE, m, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
     
-                call MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
+                call MPI_Probe(MPI_ANY_SOURCE, m, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
                 call MPI_Get_count(Status, MPI_COMPLEX8, sum_size2, Err)
 
                 allocate(MCR_comp_arr(sum_size2))
-                call MPI_Recv(MCR_comp_arr, sum_size2, MPI_COMPLEX8, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
+                call MPI_Recv(MCR_comp_arr, sum_size2, MPI_COMPLEX8, MPI_ANY_SOURCE, m, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
             end if
             allocate(MCR_ind(3*qlen))
-            call MPI_Recv(MCR_ind, 3*qlen, MPI_INTEGER, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
+            call MPI_Recv(MCR_ind, 3*qlen, MPI_INTEGER, MPI_ANY_SOURCE, m, MPI_COMM_WORLD, Status, Err) !нужен не энитег, а конкретные
 
 
 
@@ -366,9 +399,16 @@ contains
             end do
             deallocate(ij, MCR_real, MCR_comp_mat, MCR_comp_arr, MCR_ind)
 
+            ! allocate(mode_res_m(minm:maxm,1))
+            mode_res_m(1:,m) = mode_res
+            deallocate(queue, mode_res)
+
+            print*, "kek", rank, m
+            end do
 
             print*, "-------------------------------"
-            print*, status%MPI_SOURCE
+
+            ! print*, status%MPI_SOURCE
             ! print*, 2, mode_res(1)%factors%Qext
             ! print*, 2, mode_res(1)%factors%Qsca
             ! print*, 2, mode_res(1)%tmatrix(3,5)
@@ -378,52 +418,58 @@ contains
 
 
             ! m = m_array(Rank)
-            m = m_array(status%MPI_SOURCE)
+            ! m = m_array(status%MPI_SOURCE)
 
 
 
             ! добавить внешний цикл по элементам qlenЫ-queueЫ
             ! +собрать массив accuracyЫ
+            do k = 1, size(m_array)
+            m = m_array(k)
+            qlen = qlen_m(m)
             do i = 1, qlen
-                if (queue(i)%to_res) then
-                    accuracy = max(accuracy, res%update_and_get_accuracy(queue(i)%info, mode_res(i)%factors))
+
+                if (queue_m(i,m)%to_res) then
+                    accuracy = max(accuracy, res%update_and_get_accuracy(queue_m(i,m)%info, &
+                    mode_res_m(i,m)%factors))
                 endif
+            enddo
             enddo
 
             ! ! из этого тоже цикл по элементам mode_resЫ (добавить чтобы печатало 
             ! ! только до тех элементов из accuracyЫ, до которых имеет смысл)
             ! call typed_model%print_mode_row(m, mode_res)
 
-            if (need_indicatrix) then
-                ! и из этого по элементам mode_resЫ-accuracyЫ
-                    solutions = typed_model%solution_places(m)
-                    do i = 1, size(solutions)
-                        dim = size(mode_res(solutions(i)%source_tm)%solution)
-                        solution_tm(:dim,solutions(i)%m) = mode_res(solutions(i)%source_tm)%solution
-                        solution_te(:dim,solutions(i)%m) = mode_res(solutions(i)%source_te)%solution
-                        basis_types(solutions(i)%m) = solutions(i)%basis_type
+            ! if (need_indicatrix) then
+            !     ! и из этого по элементам mode_resЫ-accuracyЫ
+            !         solutions = typed_model%solution_places(m)
+            !         do i = 1, size(solutions)
+            !             dim = size(mode_res(solutions(i)%source_tm)%solution)
+            !             solution_tm(:dim,solutions(i)%m) = mode_res(solutions(i)%source_tm)%solution
+            !             solution_te(:dim,solutions(i)%m) = mode_res(solutions(i)%source_te)%solution
+            !             basis_types(solutions(i)%m) = solutions(i)%basis_type
 
-                        update = calculate_amplitude_matrix_m(solutions(i)%basis_type, scattering_context, solutions(i)%m, lnum, &
-                        theta_bucket_size, scattering_context%directions%thetas(theta_bucket_start:theta_bucket_end), &
-                        nphi, scattering_context%directions%phis, &
-                        direction_calculation, mode_res(solutions(i)%source_te)%solution, mode_res(solutions(i)%source_tm)%solution)
-                        accuracy = max(accuracy, update_amplitude_and_get_accuracy(ampl, update, ntheta, nphi))
-                    enddo
+            !             update = calculate_amplitude_matrix_m(solutions(i)%basis_type, scattering_context, solutions(i)%m, lnum, &
+            !             theta_bucket_size, scattering_context%directions%thetas(theta_bucket_start:theta_bucket_end), &
+            !             nphi, scattering_context%directions%phis, &
+            !             direction_calculation, mode_res(solutions(i)%source_te)%solution, mode_res(solutions(i)%source_tm)%solution)
+            !             accuracy = max(accuracy, update_amplitude_and_get_accuracy(ampl, update, ntheta, nphi))
+            !         enddo
 
-            endif
+            ! endif
 
             ! из этого тоже цикл по элементам mode_resЫ (добавить чтобы печатало 
             ! только до тех элементов из accuracyЫ, до которых имеет смысл, 
             ! то есть поместить нижележащую проверку на точность сюда. код был передвинут 
             ! сюда как раз для корректронсти этой проверки)
-            call typed_model%print_mode_row(m, mode_res)
+            ! call typed_model%print_mode_row(m, mode_res)
 
             ! if (size(queue) > 0 .and. accuracy < MIN_M_RATIO) then
             !     real_maxm = m
             !     exit
             ! endif
         ! end do
-        deallocate(queue, mode_res)
+        ! deallocate(queue, mode_res)
 
         end if
 
