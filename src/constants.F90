5c79384133797a7b24be2ca175e7e81b738d3da2
sergey <st079380@student.spbu.ru>
2023-03-09 02:13:49 +0300

    MPI_1.2 (accuracy, problems)

--- src/constants.F90
+++ src/constants.F90
@@ -8,7 +8,7 @@ module constants
 
     ! MPI constants
     integer, public :: Err, Rank, Size_mpi
-    TYPE(MPI_Status) :: Status
+    type(MPI_Status) :: Status
 
     real(knd), parameter :: PI = 4.0_knd * atan(1.0_knd)
     real(knd), parameter :: BASE_ACCURACY = 1e-8_knd
